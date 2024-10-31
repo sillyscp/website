b9aff308728c636800a60003b96ff72c6a6b00c2
Jay <me@jayxtq.xyz>
2024-10-31 21:23:29 +0000

    fix: again stupid classnames

--- src/routes/apply/+page.svelte
+++ src/routes/apply/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="sm:flex sm:items-center sm:justify-center m-10">
-    <div class="space-y-4 xl:w-[44rem] lg:w-[34rem] md:w-56 sm:w-24 w-16 rounded-lg border-2 border-slate-600 p-2 lg:p-10">
+    <div class="space-y-4 xl:w-[44rem] lg:w-[34rem] rounded-lg border-2 border-slate-600 p-2 lg:p-10">
         {#if closed}
             <p class="font-bold text-red-600">Applications are currently closed</p>
         {/if}
