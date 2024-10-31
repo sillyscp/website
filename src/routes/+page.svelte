84c834ba3eb6c6f00abc371f980486892075aa52
Jay <me@jayxtq.xyz>
2024-10-31 22:59:30 +0000

    feat: main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
 <div class="flex items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold">Currently a work in progress, if looking for applications head over to <a href="/apply" class="text-blue-500">the application page</a></h1>
+    <h1 class="text-4xl font-bold">Currently a work in progress</h1>
+    <ul>
+        <li><a href="/discord">Our Discord</a></li>
+        <li><a href="/apply">Apply for Discord staff</a></li>
+    </ul>
 </div>
