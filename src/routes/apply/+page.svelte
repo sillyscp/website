c25b1759d78360dbedd88f6cd741d071f527cdff
Jay <me@jayxtq.xyz>
2024-10-31 21:28:03 +0000

    style: more styling

--- src/routes/apply/+page.svelte
+++ src/routes/apply/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="flex items-center justify-center m-10">
-    <div class="space-y-4 xl:w-[44rem] lg:w-[34rem] w-full rounded-lg border-2 border-slate-600 p-2 lg:p-10">
+    <div class="space-y-4 xl:w-[44rem] lg:w-[34rem] w-full rounded-lg border-2 border-slate-600 p-2 sm:p-10 p-4">
         {#if closed}
             <p class="font-bold text-red-600">Applications are currently closed</p>
         {/if}
