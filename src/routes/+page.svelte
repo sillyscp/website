e5f61c2cbf571047f5a2d99fc2efeb100aca58ac
Jay <me@jayxtq.xyz>
2024-10-31 23:33:32 +0000

    style: space

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center h-screen">
-    <div>
+    <div class="space-y-4">
         <h1 class="text-4xl font-bold">Currently a work in progress</h1>
         <ul class="list-disc list-inside">
             <li><a href="/discord" class="text-blue-600">Our Discord</a></li>
