028aec5fbf24d636a0e0642fd9f8ba317f457584
Jay <me@jayxtq.xyz>
2024-10-31 21:22:48 +0000

    fix: classnames

--- src/routes/apply/+page.svelte
+++ src/routes/apply/+page.svelte
@@ -7,7 +7,7 @@
     export let data: PageData;
 </script>
 
-<div class="flex items-center justify-center my-10">
+<div class="sm:flex sm:items-center sm:justify-center m-10">
     <div class="space-y-4 xl:w-[44rem] lg:w-[34rem] md:w-56 sm:w-24 w-16 rounded-lg border-2 border-slate-600 p-2 lg:p-10">
         {#if closed}
             <p class="font-bold text-red-600">Applications are currently closed</p>
