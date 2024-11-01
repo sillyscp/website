6937abae9e3773a0bf13f56907378a55cba378dc
Jay <me@jayxtq.xyz>
2024-11-01 21:42:54 +0000

    fix: add 19 character long ids

--- src/routes/apply/schema.ts
+++ src/routes/apply/schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const applySchema = z.object({
     tag: z.string().min(2, "Discord tags are at least 2 characters in length").max(32, "Discord names are less than 32 characters in length"),
-    id: z.string().min(17, "Please enter a valid Discord ID").max(18, "Please enter a valid Discord ID"),
+    id: z.string().min(17, "Please enter a valid Discord ID").max(19, "Please enter a valid Discord ID"),
     age: z.number().int().min(16, "You must be at least 16 to apply, please do not lie just to apply").max(99, "No way you're above 100."),
     q1: z.string().min(10),
     q2: z.string().min(10),
