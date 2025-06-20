c119f10960556503d07d8e97f7cf328212b4cafc
Lumi <me@jxtq.moe>
2025-06-20 19:41:59 +0100

    chore: update ip:port

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
         <img src="/favicon.png" class="w-72 h-72 mx-auto" alt="Logo" />
         <h1 class="text-6xl font-bold text-center"><span class="text-[#5BCFFA]">S</span><span class="text-[#F5ABB9]">i</span><span>l</span><span class="text-[#F5ABB9]">l</span><span class="text-[#5BCFFA]">y</span> SCP</h1>
         <div class="flex gap-4 items-center justify-center !mt-10">
-            <Button onclick={() => navigator.clipboard.writeText("play.sillyscp.gay:7777")} class="bg-[#5BCFFA] text-black">Copy server IP</Button>
+            <Button onclick={() => navigator.clipboard.writeText("45.94.220.8:25544")} class="bg-[#5BCFFA] text-black">Copy server IP</Button>
             <Button href="/discord" class="bg-[#F5ABB9] text-black">Join our Discord</Button>
             <Button href="/apply" variant="secondary" class="text-black bg-white hover:bg-secondary/80">Apply for staff</Button>
         </div>
